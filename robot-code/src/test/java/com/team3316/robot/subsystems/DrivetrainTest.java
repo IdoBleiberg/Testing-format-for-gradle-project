ef5ce5225256bab2c1a40a68ad363b6550512a2a
Ido Bleiberg <idoblibarge@gmail.com>
2019-08-10 20:47:56 +0300

    Test for drivetrain

--- robot-code/src/test/java/com/team3316/robot/subsystems/DrivetrainTest.java
+++ robot-code/src/test/java/com/team3316/robot/subsystems/DrivetrainTest.java
@@ -2,23 +2,33 @@ package com.team3316.robot.subsystems;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.team3316.kit.config.ConfigException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.beans.factory.BeanFactory;
 
 public class DrivetrainTest {
-  //Drivetrain mockDrivetrain = mock(Drivetrain.class);
+  Drivetrain drivetrain ;
 
-  private Animal dog;
+  //private Animal dog;
 
   public DrivetrainTest() {
     ApplicationContext context = new ClassPathXmlApplicationContext("/ApplicationContext.xml");
-    this.dog = (Animal) context.getBean("dog");
+    try {
+      this.drivetrain = new Drivetrain();
+    } catch (ConfigException e) {
+      // TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    //this.dog = (Animal) context.getBean("dog");
   }
 
   @Test
   public void dogTest() {
-    assertEquals("danny", dog.getName());
+    //assertEquals("danny", dog.getName());
+    this.drivetrain.setMotors(0.3, 0.2);
+    assertEquals(0.3, this.drivetrain.getMotor1()); 
   }
 }
