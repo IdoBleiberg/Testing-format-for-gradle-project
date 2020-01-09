3a46688e75e417abf002b9d06fd76d23c71c5cf4
tommykark <tommyka03@gmail.com>
2020-01-09 19:24:20 +0200

    CommandGroupV2 is done! works great!

--- src/main/java/com/team3316/robot/commands/CommandGroupV2.java
+++ src/main/java/com/team3316/robot/commands/CommandGroupV2.java
@@ -2,9 +2,6 @@ package com.team3316.robot.commands;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 import com.team3316.kit.DBugLogger;
@@ -16,8 +13,9 @@ import edu.wpi.first.wpilibj.command.IllegalUseOfCommandException;
 /**
  * CommandGroupV2
  */
-public abstract class CommandGroupV2 extends Command{
+public abstract class CommandGroupV2 extends Command {
     List<List<Supplier<Command>>> queue = new LinkedList<>();
+    List<Command> parallelsList;
 
     boolean canAdd = true;
     boolean isFinished = false;
@@ -32,38 +30,39 @@ public abstract class CommandGroupV2 extends Command{
     @Override
     public synchronized void start() {
         canAdd = false;
-        ExecutorService es = null;
-        for (List<Supplier<Command>> list : queue) {
-            es =  Executors.newCachedThreadPool();
-            DBugLogger.getInstance().info("SEQUENTIAL HAS STARTED");
-            for (Supplier<Command> sup : list) {
-                es.submit(() -> {
-                    Command cmd = sup.get();
-                    cmd.start();
-                    boolean wasRunning = false;
-                    while (!(!cmd.isRunning() || wasRunning)) {
-                      DBugLogger.getInstance().info("INWHILE: " + cmd.isRunning() + ", " + wasRunning);
-                      if (cmd.isRunning()) wasRunning = true;
-                    }
-                });
-            }
-            es.shutdown();
-            try {
-              es.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
-              DBugLogger.getInstance().info("SEQUENTIAL HAS ENDED BEFORE TIMEOUT");
-            } catch (InterruptedException e) {
-              DBugLogger.getInstance().info("SEQUENTIAL HASN'T ENDED UNTIL TIMEOUT");
-            }
-        }
-        isFinished = true;
+        super.start();
     }
 
+
+    @Override
+    protected void execute() {
+      if (parallelsList == null) {
+        this._runNextSequential();
+      } else if (parallelsList.stream().allMatch(cmd -> cmd.isCompleted())) {
+        DBugLogger.getInstance().info(parallelsList.toString());
+        if (!queue.isEmpty()) {
+          this._runNextSequential();
+        } else {
+          isFinished = true;
+        }
+      }
+    }
+
+    private void _runNextSequential() {
+      List<Supplier<Command>> list = queue.remove(0);
+      parallelsList = new LinkedList<>();
+      for (Supplier<Command> sup : list) {
+        Command cmd = sup.get();
+        cmd.start();
+        parallelsList.add(cmd);
+      }
+    }
     /**
      * @param args - appends a sequence of commands to run. Given as suppliers, each returning a new command instance. These will run simultaneously.
      *
      */
     @SafeVarargs
-    public final synchronized void add(Supplier<Command>... args) throws ConfigException{
+    public final synchronized void add(Supplier<Command>... args) throws ConfigException {
         List<Supplier<Command>> l = new LinkedList<>();
 
         for (int i = 0; i < args.length; i++) {
